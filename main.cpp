4856504bc6f89749c9364d80649c2c720605d55b
up202009029 <78442564+up202009019@users.noreply.github.com>
2021-04-20 21:24:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,159 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 //Function for the main menu returns the option chosen by the user
-int game_start(){
-  // variable to store the user input
-  int option;
+int game_start() {
+    // variable to store the user input
+    int option;
 
-  //shows the menu
-  cout << "1) Rules;" << endl << "2) Play;" << endl << "0) Exit" << endl;
+    //shows the menu
+    cout << "1) Rules;" << endl << "2) Play;" << endl << "0) Exit" << endl;
 
-  //asks repeatedly for an option only accpeting 1, 2 or 0
-  do{
-  cin >> option;
-  if(option != 1 & option != 2 & option != 0){
-    cout << "Input not valid, please try again." << endl;
-  }
-  }while(option != 1 & option != 2 & option != 0);
-  return option;
+    //asks repeatedly for an option only accpeting 1, 2 or 0
+    do {
+        cin >> option;
+        if (option != 0 & option != 1 & option != 2) {
+            cout << "Input not valid, please try again." << endl;
+        }
+    } while (option != 0 & option != 1 & option != 2);
+    return option;
+}
+
+bool teste(int maze_num) {
+
+    bool flag = false;
+
+    string maze;
+
+    if (maze_num < 10) {
+        maze = "Maze_0" + to_string(maze_num) + ".TXT";
+    }
+    else {
+        maze = "Maze_" + to_string(maze_num) + ".TXT";
+    }
+
+    ifstream mazefile;
+    mazefile.open(maze);
+    if (mazefile.fail()) {
+        flag = true;
+    }
+
+    return flag;
+}
+
+void maze(int maze_num, int &player_line, int &player_col) {
+
+    string player;
+    int count = -1;
+
+    string maze;
+
+    if (maze_num < 10) {
+        maze = "Maze_0" + to_string(maze_num) + ".TXT";
+    }
+    else {
+        maze = "Maze_" + to_string(maze_num) + ".TXT";
+    }
+
+    ifstream mazefile;
+    mazefile.open(maze);
+
+    //linhas ficheiro
+    string line;
+    int l, c;
+
+    while (getline(mazefile, line)) {
+        cout << line << endl;
+        if (count == 0) {
+            l = line.length();
+        }
+
+        count++;
+
+    }
+    mazefile.close();
+
+    mazefile.open(maze);
+
+    c = count;
+    char ** pos = new char*[l];
+
+    for (int i = 1; i < l; i++) {
+        pos[i] = new char[c];
+    }
+
+    //estava a tentar criar uma matriz para guardar as posições dos objetos mas ainda não consegui 
+    while (getline(mazefile,line)) {
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                pos[i][j] = line[i];
+                if (line[i] == 'H') {
+                    player_line = i;
+                    player_col = j;
+                }
+            }
+        }
+    }
+    mazefile.close();
+
+    //Return to main menu
+    if (maze_num == 0) {
+        game_start();
+    }
+}
+
+void moves() {
+    char x;
+
+    cout << endl << "Where do you want to go?" << endl;
+    cin >> x;
+
+
+}
+
+void play(int maze_num) {
+
+    string player_position;
+    int player_line, player_col;
+
+    maze(maze_num, player_line, player_col);
+
+    cout << "Player: " << endl << "line: " << player_line << endl << "column: " << player_col;
+
+    moves();
 }
 
 int main() {
 
-  // variables user input and chosen maze
-  int option = game_start();
-  int maze;
+    // variables user input and chosen maze
+    int option = game_start();
+    int maze_num;
 
-  //actions regarding the option chose by the user
+    //actions regarding the option chose by the user
 
-  //rules
-  if(option == 1){
-    cout << "The rules are: ";
-  }
-  //Play the game
-  else if(option == 2){
-
-    cout << "Here we go!" << endl << "Choose the maze: ";
-    //Choose an available maze
-    do{
-      cin >> maze;
-    }while(maze != 0);
-
-    //Return to main menu
-    if(maze == 0){
-      game_start();
+    //rules
+    if (option == 1) {
+        cout << "The rules are: ";
     }
-  }
-  // Exit
-  else{
-    cout << "Bye!";
-  }
-  
+    //Play the game
+    else if (option == 2) {
+
+        cout << "Here we go!" << endl << "Choose the maze: ";
+        //Choose an available maze
+        do {
+            cin >> maze_num;
+        } while (maze_num != 01 & teste(maze_num)); //test if the maze exists
+
+        if (maze_num != 0) {
+            play(maze_num);
+        }
+        // Exit
+        else {
+            cout << "Bye!";
+        }
+    }
+
 }
