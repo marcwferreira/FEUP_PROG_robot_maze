ff6dca6ea7f351149a1b9a348b5c23ddb93ea0c0
sneinhnz <72470150+sneinzeinhnz@users.noreply.github.com>
2021-04-22 17:37:13 +0100

    Fiz mais alguns teste para o movimento e adicionei comecei a fazer as "frameworks" para os robos

--- position.cpp
+++ position.cpp
@@ -74,9 +74,17 @@ void maze_load() {
 }
 */
 
+//create a structure for the robots
+struct robot{
+        int condition;
+        int pos_y;
+        int pos_x;
+    };
+    
 int main() {
 
     int count = -1;
+    int countrobots = 0;
 
     int playerx, playery;
 
@@ -88,11 +96,19 @@ int main() {
     int posx, posy;
 
     while (getline(mazefile, line)) {
+
         //cout << line << endl;
         if (count == 0) {
             posx = line.length();
         }
 
+        //count the number of robots
+        int line_len = line.length();
+        for(int i=0; i < line_len; i++){
+            if(line[i] == 'R'){
+                countrobots++;
+            }
+        }
         count++;
 
     }
@@ -105,9 +121,14 @@ int main() {
 
     char maze_map[posy][posx]; //creating a 2d char array to store the map
 
+    struct robot rob[countrobots];
+
+//_________________________________________________________________________________
+
      mazefile.open("maze_01.txt");
 
     int count2 = 0; //counting the lines
+    int robot_index;
 
     while(getline(mazefile,line)){
         for(int i=0; i < posx; i++){
@@ -125,10 +146,17 @@ int main() {
         for(int j=0;j<posx;j++){
             cout << maze_map[i][j];
 
+            //find player position in the map
             if(maze_map[i][j] == 'H'){
                 playerx = j;
                 playery = i;
             }
+
+            //find robots and assign IDs
+            if(maze_map[i][j] == 'R'){
+                //add robot funtion
+            }
+
         }
         cout << endl;
     }
@@ -144,14 +172,53 @@ int main() {
     cin >> movement;
 
     if(movement == 'w'){
-        maze_map[playery-1][playerx] = 'H';
-        maze_map[playery][playerx] = ' ';
-        playery--;
+        if(maze_map[playery-1][playerx] == '*' || maze_map[playery-1][playerx] == 'r'){
+            cout << "Movement is invalid, please select another movement";
+        }
+        else{
+            maze_map[playery-1][playerx] = 'H';
+            maze_map[playery][playerx] = ' ';
+            playery--;
+        }
+    }
+
+    if(movement == 'a'){
+        if(maze_map[playery][playerx-1] == '*' || maze_map[playery][playerx-1] == 'r'){
+            cout << "Movement is invalid, please select another movement";
+        }
+        else{
+            maze_map[playery][playerx-1] = 'H';
+            maze_map[playery][playerx] = ' ';
+            playerx--;
+        }
+    }
+
+    if(movement == 's'){
+        if(maze_map[playery+1][playerx] == '*' || maze_map[playery+1][playerx] == 'r'){
+            cout << "Movement is invalid, please select another movement";
+        }
+        else{
+            maze_map[playery+1][playerx] = 'H';
+            maze_map[playery][playerx] = ' ';
+            playery++;
+        }
+    }
+
+    if(movement == 'd'){
+        if(maze_map[playery][playerx+1] == '*' || maze_map[playery][playerx+1] == 'r'){
+            cout << "Movement is invalid, please select another movement";
+        }
+        else{
+            maze_map[playery][playerx+1] = 'H';
+            maze_map[playery][playerx] = ' ';
+            playerx++;
+        }
     }
 
     //testing movement
     cout << "testing movement" << endl;
 
+    system("cls"); //windows specif, just for testing
     for(int i=0;i<posy;i++){
         for(int j=0;j<posx;j++){
             cout << maze_map[i][j];
