8eff57ad78c2061ad4a64f9ed9a448ec75c7e91b
sneinhnz <72470150+sneinzeinhnz@users.noreply.github.com>
2021-04-22 15:21:23 +0100

    Started with player position, just a sketch.

--- position.cpp
+++ position.cpp
@@ -9,6 +9,70 @@
 #include <iterator>
 
 using namespace std;
+/*
+void maze_load() {
+    int count = -1;
+
+    int playerx, playery;
+
+    ifstream mazefile;
+    mazefile.open("maze_01.txt");
+
+    //linhas ficheiro
+    string line;
+    int posx, posy;
+
+    while (getline(mazefile, line)) {
+        //cout << line << endl;
+        if (count == 0) {
+            posx = line.length();
+        }
+
+        count++;
+
+    }
+    mazefile.close();
+
+    posy = count + 1;
+
+    //posx is the number of columns
+    //posy is the number of lines
+
+    char maze_map[posy][posx]; //creating a 2d char array to store the map
+
+     mazefile.open("maze_01.txt");
+
+    int count2 = 0; //counting the lines
+
+    while(getline(mazefile,line)){
+        for(int i=0; i < posx; i++){
+            maze_map[count2][i] = line[i];
+        }
+        count2++;
+    }
+
+    mazefile.close(); //close map file
+
+    //uset to test if map was put correctly
+    cout << "testing map" << endl;
+
+    for(int i=0;i<posy;i++){
+        for(int j=0;j<posx;j++){
+            cout << maze_map[i][j];
+
+            if(maze_map[i][j] == 'H'){
+                playerx = j;
+                playery = i;
+            }
+        }
+        cout << endl;
+    }
+
+    //testing player position
+    cout << "x y:" << playerx << " " << playery << endl;
+
+}
+*/
 
 int main() {
 
@@ -24,7 +88,7 @@ int main() {
     int posx, posy;
 
     while (getline(mazefile, line)) {
-        cout << line << endl;
+        //cout << line << endl;
         if (count == 0) {
             posx = line.length();
         }
@@ -36,7 +100,10 @@ int main() {
 
     posy = count + 1;
 
-    char maze_map[posy][posx];
+    //posx is the number of columns
+    //posy is the number of lines
+
+    char maze_map[posy][posx]; //creating a 2d char array to store the map
 
      mazefile.open("maze_01.txt");
 
@@ -49,22 +116,49 @@ int main() {
         count2++;
     }
 
-    cout << "test" << endl;
+    mazefile.close(); //close map file
+
+    //uset to test if map was put correctly
+    cout << "testing map" << endl;
 
     for(int i=0;i<posy;i++){
         for(int j=0;j<posx;j++){
-            cout << maze_map[i][j] << endl;
+            cout << maze_map[i][j];
 
             if(maze_map[i][j] == 'H'){
                 playerx = j;
                 playery = i;
             }
         }
+        cout << endl;
     }
 
+    //testing player position
     cout << "x y:" << playerx << " " << playery << endl;
 
+    //__________________________________________________________
+    while(true){
+    //player movement draft
+    char movement;
+    cout << "choose a movement" << endl;
+    cin >> movement;
 
+    if(movement == 'w'){
+        maze_map[playery-1][playerx] = 'H';
+        maze_map[playery][playerx] = ' ';
+        playery--;
+    }
+
+    //testing movement
+    cout << "testing movement" << endl;
+
+    for(int i=0;i<posy;i++){
+        for(int j=0;j<posx;j++){
+            cout << maze_map[i][j];
+        }
+        cout << endl;
+    }
+    }
 
     return 0;
 }
