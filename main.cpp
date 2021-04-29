d2c20878ec544bda99306179f2cd866a9b6f1214
sneinhnz <72470150+sneinzeinhnz@users.noreply.github.com>
2021-04-29 12:18:40 +0100

    Recording player time and organizing it is done. Still has a bug for the player name.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <chrono>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -520,14 +521,36 @@ int play(int maze_num,game_info &gameplay) {
     return player_time;
 }
 
+//function to determine sort function of a 2d vector - used in player_win
+bool sort_winner_time( const vector<string>& v1, const vector<string>& v2 ) {
+
+    const int player_time = 1;
+
+    //comparing the times
+    bool v1_less_than_v2 = v1[player_time] < v2[player_time];
+
+    return v1_less_than_v2;
+}
+
 //Player won - keep the time of his game
 void player_win(int game_time, int maze_num){
 
     const string save_yes = "1";
     const string save_no = "0";
+    const int player_name_read = 0;
+    const int player_time_read = 1;
     const int max_name_size = 15;
 
+    vector<string> reading_file; //help with reading file strings
+    vector<string> writing_to_file; //help to put the name in the file if it doesnt exist
+    vector<vector<string>> winner_reading;//used to sort winners if file exists
+
+    stringstream player_name_to_store;
+    stringstream player_time_to_store;
     string save_time_choice;
+
+    bool file_already_existed = false;
+    bool player_already_exist = false;
 
     //prints the player time
     cout << "Total time to complete maze: " << game_time << "s" << endl;
@@ -552,17 +575,15 @@ void player_win(int game_time, int maze_num){
         string player_name;
 
         //ask player name also only allow for names up to 15 chars
-        cout << "Please choose name to be recorded (name must have a max of 15 character, spaces are allowed but count as a character): ";
+        cout << "Please choose name to be recorded (name must have a max of 15 character): ";
        do{
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             getline(cin, player_name);
             if(cin.eof()){
                 std::exit(0);
-            
             }
             else if(player_name.size() > max_name_size){ //check if player tried to input more than 15 chars
                 cout << "Name must have a max of 15 chars." << endl << "Please choose name to be recorded: ";
-                cin.clear();
             }
         } while(player_name.size() > max_name_size);
 
@@ -572,6 +593,8 @@ void player_win(int game_time, int maze_num){
         //opening the file
         ifstream mazefile;
         mazefile.open(maze_select);
+
+        string line;//used to read strings of the file
 
         //create a winner file for the maze if it doesn't exist
         if(!mazefile.is_open()){
@@ -585,8 +608,68 @@ void player_win(int game_time, int maze_num){
         else{
             
             //STORE AND ORGANIZE WINNERS
+
+            //ignore the first two lines of the file
+            mazefile.ignore ( std::numeric_limits<std::streamsize>::max(), '\n' );
+            mazefile.ignore ( std::numeric_limits<std::streamsize>::max(), '\n' );
+
+            //separating the names and times into a vector
+            while(getline(mazefile,line)){
+                reading_file.clear(); //clear the previous string info
+                reading_file.resize(2); //resize reading_file to store name and time
+                reading_file[player_name_read] = line.substr (0, 14); //store name in reading_file
+                reading_file[player_time_read] = line.substr(16, 8); //store time in reading_file (with some leniance)
+                winner_reading.push_back(reading_file);//storing the pair name and time in winner_reading
+            }
+
+            //preparing data to go into vector
+            player_name_to_store << left << setw(14) << player_name;
+            player_time_to_store << right << setw(5) << game_time << "s";
+
+            //if name alreay exist replace the time
+            for(int index = 0; index < winner_reading.size(); index++){
+                if(winner_reading[index][player_name_read] == player_name_to_store.str()){
+                    winner_reading[index][player_time_read] = player_time_to_store.str();
+                    player_already_exist = true;
+                }
+            }
+            //if name is not already in the file it adds it
+            if(!player_already_exist){
+                //preaparing the vector to add to winnner_reading
+                writing_to_file.push_back(player_name_to_store.str());
+                writing_to_file.push_back(player_time_to_store.str());
+                //adding the vector to wiiner_reading
+                winner_reading.push_back(writing_to_file);
+            }
+
+            //sorting the order by the time - using function sort_winner_time
+            sort(winner_reading.begin(), winner_reading.end(),sort_winner_time);
+            
+            //set true to file_already_existed
+            file_already_existed = true;
+
+            for(int i = 0; i < winner_reading.size(); i++){
+                cout << "test: " << winner_reading[i][player_name_read] << " e " << winner_reading[i][player_time_read] << endl;
+            }
         
         }  mazefile.close();
+
+        //writting the altered data to the winner file if file existed already
+        if(file_already_existed){
+            ofstream mazefile;
+            mazefile.open (maze_select);
+            if(!mazefile.is_open()){
+                cout << "ERROR!" << endl;
+            }
+            else{
+                mazefile << "Player         -  Time" << endl;
+                mazefile << "----------------------" << endl;
+                for(int index = 0; index < winner_reading.size(); index++){
+                    mazefile << left << setw(15) <<winner_reading[index][player_name_read] << "-" << right << setw(6) << winner_reading[index][player_time_read] << endl; 
+                }
+
+            } mazefile.close();
+        }
 
          //display a message showing time was saved
         cout << "Time saved, if you wish to see it go to time records!" << endl;
